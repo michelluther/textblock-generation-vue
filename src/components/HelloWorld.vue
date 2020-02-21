b7de5f97e066b96c8676dc4816255f78a4a63756
Michael Luther <michel.luther@gmail.com>
2020-02-21 08:23:12 +0100

    feat(): textblock parsing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,120 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
+    <h1>Your favorites recipes</h1>
+    <div v-for="textBlock in this.textBlocks" v-bind:key="textBlock.id">
+      <div>
+      {{textBlock.text}}
+      </div>
+    </div>
+    <mugen-scroll :handler="getRecipes" :should-handle="!loading">
+    </mugen-scroll>
   </div>
 </template>
 
 <script>
+const axios = require('axios')
+  import MugenScroll from 'vue-mugen-scroll'
+
+const extractTextBlocks = (text) => {
+
+  const currentTextBoxMatrix = {};
+
+  let currentColumns = [];
+  const textBlocks = [];
+
+  const addTextBlock = textBlock => {
+    textBlocks.push(textBlock)
+    currentTextBoxMatrix[`textBlock_${textBlock.startColumn}`]
+  }
+
+  const getTextBlock = column => {
+    return currentTextBoxMatrix[`textBlock_${column}`]
+  }
+
+  const buildTextBlock = (startLine, startColumn, endColumn) => {
+    return {
+      startLine: startLine,
+      endLine: startLine,
+      startColumn: startColumn,
+      endColumn: endColumn,
+      text: ''
+    }
+  }
+  const findColumns = (lineOfText) => {
+    // eslint-ignore 
+    console.log(lineOfText) // eslint-disable-line
+    const columns = [];
+    let numberOfConcurrentSpaces = 0;
+    for (var i = 0; i < lineOfText.length; i++) {
+      if(lineOfText.charAt(i) === ' '){
+        numberOfConcurrentSpaces ++
+      }
+      else if(lineOfText.charAt(i) !== ' ' && numberOfConcurrentSpaces > 2) {
+        columns.push(
+            {startColumn: i
+          })
+        columns[columns.length - 1].endColumn ++
+      }
+    }
+    return columns
+  }
+
+  const lines = text.split('\n');
+
+  lines.forEach(line => {
+    let thisLinesColumns = findColumns(line)
+    thisLinesColumns.forEach( (column, index) => {
+      if(column.startColumn === currentColumns[index].startColumn){
+        getTextBlock(column.startColumn)
+      } else {
+        addTextBlock(buildTextBlock(column.startLine, column.startColumn, column.endColumn))
+      }
+    })
+    currentColumns = thisLinesColumns
+  })
+
+  return textBlocks
+}
+
 export default {
   name: 'HelloWorld',
+  components: {
+    MugenScroll
+  },
+  data() {
+    return {
+      recipes: [],
+      page: 0,
+      loading: false,
+      recipeContent: '',
+      textBlocks: [{id:1, text: 'hahah'}]
+    }
+  },
   props: {
     msg: String
+  },
+  methods:{
+    getRecipes(){
+      this.page ++;
+      this.loading = true;
+      axios.get(`http://localhost:8080/api/result_frikadellen.txt`, {
+        page: this.page
+      })
+          .then((response)=>{
+            // myThat.recipes = [] 
+            // console.log(response.data);
+            this.recipeContent = response.data;
+            this.textBlocks = extractTextBlocks(this.recipeContent)
+            this.loading = false;
+          }
+      )
+    }
+  },
+
+  mounted: function() {
+    // var myThat = this;
+    // this.getRecipes();
   }
 }
 </script>
@@ -49,8 +129,10 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  width: 200px;
+  border: 1px solid #aaa;
+  padding: 40px 10px;
+  margin: 5px;
 }
 a {
   color: #42b983;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import ScrollLoader from 'vue-scroll-loader'
 
 Vue.config.productionTip = false
+// Vue.component('scroll-loader', ScrollLoader)
 
 new Vue({
   render: h => h(App),
